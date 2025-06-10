5fd2e545fb4c77ef9a3764f615fc709a38b198b3
EstudianteMartinez <l21390264@chetumal.tecnm.mx>
2025-06-10 12:05:16 -0500

    puerto 5174

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: 5174 // Aquí pones el puerto que tú quieras
+  }
 })
