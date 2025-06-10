00659cfb8b32d78f65793f1cc70ed55575e3dfd8
Rusell <emanuenrusell@gmail.com>
2025-06-10 12:59:24 -0500

    feat: agregar componentes CargarIndicadores y CrearIndicador, y configurar rutas en el enrutador

---
+++ src/components/Cargarindicadores.vue
@@ -0,0 +1,1 @@
+<template>Buenas Tardes</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,12 @@ import Login from '../views/Login.vue'
 import PanelDeControl from '../components/PanelDeControl.vue'
 import Indicador from '../components/Indicador.vue'
 import VerIndicadores from '../components/VerIndicadores.vue'
+import Cargarindicadores from '@/components/Cargarindicadores.vue'
+import CrearIndicador from '@/components/CrearIndicador.vue'
 import Plantillas from '../components/Plantillas.vue'
 import Documentos from '../components/Documentos.vue'
 import Usuarios from '../components/Usuarios.vue'
 import Ejes from '../components/Ejes.vue'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,29 +28,39 @@ const router = createRouter({
           name: '/Indicador',
           component: Indicador,
           children: [
-                {
-              path: '/VerIndicadores',
-              name: '/VerIndicadores',
+            {
+              path: 'ver-indicadores',
+              name: 'ver-indicadores',
               component: VerIndicadores,
-                }
+            },
+            {
+              path: 'CargarIndicador',
+              name: 'CargarIndicador',
+              component: Cargarindicadores,
+            },
+            {
+              path: 'CrearIndicador',
+              name: 'CrearIndicador',
+              component: CrearIndicador,
+            },
           ],
         },
-            {
+        {
           path: '/Plantillas',
           name: 'Plantillas',
           component: Plantillas,
         },
-                {
+        {
           path: '/Documentos',
           name: 'Documentos',
           component: Documentos,
         },
-                    {
+        {
           path: '/Ejes',
           name: 'Ejes',
           component: Ejes,
         },
-                {
+        {
           path: '/Usuarios',
           name: 'Usuarios',
           component: Usuarios,
