82a6ed6813b8eb95d5f911ba685d6a395a184621
EstudianteMartinez <l21390264@chetumal.tecnm.mx>
2025-07-04 14:06:11 -0500

    .

--- src/directives/SoloTextoYNumeros.js
+++ src/directives/SoloTextoYNumeros.js
@@ -2,8 +2,8 @@ export default {
   beforeMount(el) {
     el.addEventListener("input", () => {
       const val = el.value;
-      // Permite letras, números, espacios y caracteres comunes en correos: @ . _ - +
-      const clean = val.replace(/[^a-zA-Z0-9 @._\-+]/g, '');
+      // Permite letras (incluyendo acentuadas y ñ), números, espacios y caracteres comunes en correos: @ . _ - +
+      const clean = val.replace(/[^a-zA-Z0-9 áéíóúÁÉÍÓÚñÑ@._\-+]/g, '');
       if (val !== clean) {
         el.value = clean;
         el.dispatchEvent(new Event("input", { bubbles: true }));
